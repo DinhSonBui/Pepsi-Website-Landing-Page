56764c777308b8b230b172f3766c24baa0446dfb
Dinh Son <buidinhson1209@gmail.com>
2022-08-22 09:18:48 +0700

    add all file

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function changeImg (img){
+    document.querySelector('.pepsi').src = img;
+}
+
+function changeBg (color){
+    const sec = document.querySelector('.sec');
+    sec.style.background = color;
+}
